6203f61bfba29d8fbbbf01fae315c7b7b538e3bd
jv <jeff@jeffvier.com>
2015-03-03 10:53:12 -0800

    syntax compression.  More variables.

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -61,14 +61,15 @@ define mediawiki::instance (
   $admin_email             = $mediawiki::admin_email
   $db_root_password        = $mediawiki::db_root_password
   $server_name             = $mediawiki::server_name
+  $doc_root                = $mediawiki::doc_root
   $mediawiki_install_path  = $mediawiki::mediawiki_install_path
   $mediawiki_conf_dir      = $mediawiki::params::conf_dir
-  $doc_root                = $mediawiki::doc_root
   $mediawiki_install_files = $mediawiki::params::installation_files
   $apache_daemon           = $mediawiki::params::apache_daemon
 
-  $vh_doc_root             = "${doc_root}/${subdir_name}"
-  $scriptpath              = "/${subdir_name}"
+  $wiki_doc_dir            = "${doc_root}/${subdir_name}"
+  $wiki_conf_dir           = "${mediawiki_conf_dir}/${name}"
+  $wiki_url_path           = "/${subdir_name}"
 
   # Figure out how to improve db security (manually done by
   # mysql_secure_installation)
@@ -81,7 +82,7 @@ define mediawiki::instance (
                         --pass puppet                             \
                         --email ${admin_email}                    \
                         --server http://${server_name}            \
-                        --scriptpath ${scriptpath}                \
+                        --scriptpath ${wiki_url_path}             \
                         --dbtype mysql                            \
                         --dbserver localhost                      \
                         --installdbuser root                      \
@@ -89,10 +90,10 @@ define mediawiki::instance (
                         --dbname ${db_name}                       \
                         --dbuser ${db_user}                       \
                         --dbpass ${db_password}                   \
-                        --confpath ${mediawiki_conf_dir}/${name}  \
+                        --confpath ${wiki_conf_dir}               \
                         --lang en",
-        creates     => "${mediawiki_conf_dir}/${name}/LocalSettings.php",
-        subscribe   => File["${mediawiki_conf_dir}/${name}/images"],
+        creates     => "${wiki_conf_dir}/LocalSettings.php",
+        subscribe   => File["${wiki_conf_dir}/images"],
       }
 
       # Ensure resource attributes common to all resources
@@ -104,48 +105,50 @@ define mediawiki::instance (
       }
         
       file {
-        "${mediawiki_conf_dir}/${name}": ;
+        $wiki_conf_dir: ;
 
-        "${mediawiki_conf_dir}/${name}/images":
+        "${wiki_conf_dir}/images":
           group  => $::operatingsystem ? {
             /(?i)(redhat|centos)/ => 'apache',
             /(?i)(debian|ubuntu)/ => 'www-data',
             default               => undef,
           };
 
-        $doc_root:
-          ensure  => "${mediawiki_conf_dir}/${name}",
-          require => File["${mediawiki_conf_dir}/${name}"];
+        $wiki_doc_dir:
+          ensure  => $wiki_conf_dir,
+          require => File[$wiki_conf_dir];
       }
       
       # Ensure that mediawiki configuration files are included in each instance.
-      mediawiki::symlinks { $name:
-        conf_dir      => $mediawiki_conf_dir,
-        install_files => $mediawiki_install_files,
-        target_dir    => $mediawiki_install_path,
+      mediawiki::symlinks {
+        $name:
+          conf_dir      => $mediawiki_conf_dir,
+          install_files => $mediawiki_install_files,
+          target_dir    => $mediawiki_install_path,
       }
      
       # Each instance has a separate vhost configuration
-      apache::vhost { $name:
-        port          => $port,
-        docroot       => $vh_doc_root,
-        serveradmin   => $admin_email,
-        servername    => $server_name,
-        vhost_name    => $ip,
-        serveraliases => $server_aliases,
-        ensure        => $ensure;
+      apache::vhost {
+        $name:
+          port          => $port,
+          docroot       => $doc_root,
+          serveradmin   => $admin_email,
+          servername    => $server_name,
+          vhost_name    => $ip,
+          serveraliases => $server_aliases,
+          ensure        => $ensure;
       }
     }
     'deleted': {
       
       file {
-        "${mediawiki_conf_dir}/${name}": # Remove the MediaWiki instance directory
+        $wiki_conf_dir: # Remove the MediaWiki instance directory
           ensure  => absent,
           recurse => true,
           purge   => true,
           force   => true;
 
-        $vh_doc_root: # Remove the symlink for the mediawiki instance directory
+        $wiki_doc_dir: # Remove the symlink for the mediawiki instance directory
           ensure   => absent,
           recurse  => true;
       }

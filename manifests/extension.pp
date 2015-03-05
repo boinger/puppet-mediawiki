591303c5498d65bc33e5ea2df9ae7851d72629f4
jv <jeff@jeffvier.com>
2015-03-05 10:55:03 -0800

    mediawiki::extension

---
+++ manifests/extension.pp
@@ -0,0 +1,50 @@
+# Define: mediawiki::extension
+#
+# This defined type allows management of extensions
+#
+# === Parameters
+#
+# [*instance*]      - instance getting the extension
+# [*localpath*]     - local path where we'll find it
+#
+# === Examples
+#
+# class { 'mediawiki':
+#   server_name      => 'www.example.com',
+#   admin_email      => 'admin@puppetlabs.com',
+#   db_root_password => 'really_really_long_password',
+#   max_memory       => '1024'
+# }
+#
+# mediawiki::instance { 'my_wiki1':
+#   db_name     => 'wiki1_user',
+#   db_password => 'really_long_password',
+# }
+#
+## === Authors
+#
+# Jeff Vier <jeff@jeffvier.com>
+#
+# === Copyright
+#
+# Copyright 2015 Jeff Vier
+#
+define mediawiki::extension (
+  $instance,
+  $localpath,
+  ) {
+
+  include mediawiki::params
+  Mediawiki::Instance[$name] -> Mediawiki::Extension[$name]
+
+  $ext_dir = "${mediawiki_conf_dir}/${instance}/extensions"
+
+  file {
+    "${ext_dir}/${name}":
+      source  => "${localpath}/${name}",
+      recurse => remote,
+      force => true,
+      require => Mediawiki::Instance[$instance];
+  }
+
+}
